66a4a8507d185a67eb445080ca00976a7f6884a1
Hamid Ranjbar <123825536+ranjbar-hamid@users.noreply.github.com>
2025-02-17 19:03:33 +0330

    Add files via upload

---
+++ _astro/Authentication.DZmV8Sq0.js
@@ -0,0 +1,1 @@
+import{A as m}from"./Authentication.BrGMfL4b.js";import"./index.C4mD4Gko.js";export{m as Auth};
---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "tix-v1";
+const urlsToCache = ["/", "/styles.css", "/app.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
